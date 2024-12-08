2a1d156d03b81b3c974e185f2a07ad7779678b74
kevinl45 <kevinlokoka@gmail.com>
2024-12-08 16:29:05 +0100

    debut frontent

---
+++ frontend/src/app/pages/add-gallery/add-gallery.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-gallery',
+  imports: [],
+  templateUrl: './add-gallery.component.html',
+  styleUrl: './add-gallery.component.scss'
+})
+export class AddGalleryComponent {
+
+}
---
+++ frontend/src/app/pages/update-gallery/update-gallery.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-gallery',
+  imports: [],
+  templateUrl: './update-gallery.component.html',
+  styleUrl: './update-gallery.component.scss'
+})
+export class UpdateGalleryComponent {
+
+}
